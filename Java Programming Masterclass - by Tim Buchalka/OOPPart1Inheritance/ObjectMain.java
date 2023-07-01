d6241ff77b0a27adb5e7e4777b446ac87c8c2aac
Om Wagh <omwaghwaghpatta@gmail.com>
2023-07-01 16:56:38 +0530

    01/07/2023

--- Java Programming Masterclass - by Tim Buchalka/OOPPart1Inheritance/ObjectMain.java
+++ Java Programming Masterclass - by Tim Buchalka/OOPPart1Inheritance/ObjectMain.java
@@ -1,48 +1,47 @@
-public class Main extends Object{
+public class ObjectMain extends Object {
 
     public static void main(String[] args) {
 
         Student max = new Student("Max", 21);
         System.out.println(max);
 
-        PrimarySchoolStudent jimmy = new PrimarySchoolStudent("Jimmy",8,
-                                            "Carol");
+        PrimarySchoolStudent jimmy = new PrimarySchoolStudent("Jimmy", 8,
+                "Carol");
         System.out.println(jimmy);
     }
 }
 
-class Student{
+class Student {
 
     private String name;
-    private  int age;
+    private int age;
 
-    Student(String name, int age){
+    Student(String name, int age) {
         this.name = name;
         this.age = age;
     }
 
-//    @Override
-//    public String toString() {
-//        return super.toString();
-//    }
-
+    // @Override
+    // public String toString() {
+    // return super.toString();
+    // }
 
     @Override
     public String toString() {
-        return (name+" is "+age);
-//        return "Student{" +
-//                "name='" + name + '\'' +
-//                ", age=" + age +
-//                '}';
+        return (name + " is " + age);
+        // return "Student{" +
+        // "name='" + name + '\'' +
+        // ", age=" + age +
+        // '}';
     }
 }
 
-class PrimarySchoolStudent extends Student{
+class PrimarySchoolStudent extends Student {
 
     private String parentName;
 
-    public PrimarySchoolStudent(String name, int age, String parentName){
-        super(name,age);
+    public PrimarySchoolStudent(String name, int age, String parentName) {
+        super(name, age);
         this.parentName = parentName;
     }
 
